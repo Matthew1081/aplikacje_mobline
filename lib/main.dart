c7803a8af35ecefda558700db233858dd736cab7
Matthew1081 <m.kwiatkowski1078@gmail.com>
2024-11-30 16:53:57 +0100

    hj

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:dsw_527/views/login/login_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: LoginView(),
+  ));
+}
+
+
+
